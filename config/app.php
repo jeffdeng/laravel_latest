2f3a05eb4f11a1f9782d4ef78d9decebaafe7eba
didibird <251040871@qq.com>
2015-10-30 16:35:39 +0800

    Intervention.php

--- config/app.php
+++ config/app.php
@@ -144,7 +144,12 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+    		
+    		/*
+    		 * non-system  Providers...
+    	    */
+    		Intervention\Image\ImageServiceProvider::class,
+    		
     ],
 
     /*
@@ -193,6 +198,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+    		
+    		/*
+    		 * non-system  Providers...
+    		 */
+    		'Image'     => Intervention\Image\Facades\Image::class,
 
     ],
 
